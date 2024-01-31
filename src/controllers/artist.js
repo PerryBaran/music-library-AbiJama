67abf9326e2949bde08d9e8b1029bd37f6022b79
Abi Jama <abi_jama88@hotmail.com>
2024-01-31 21:16:42 +0000

    successfully passed the delete artist test

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -77,9 +77,28 @@ const updateArtist = async (req, res) => {
   }
 };
 
+const deleteArtist = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query("DELETE FROM Artists WHERE id = $1 RETURNING *", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.messsage);
+  }
+};
+
 module.exports = {
   createArtist,
   readArtist,
   getArtistbyId,
   updateArtist,
+  deleteArtist,
 };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -8,4 +8,5 @@ artistRouter.get("/", artistController.readArtist);
 artistRouter.get("/:id", artistController.getArtistbyId);
 artistRouter.put("/:id", artistController.updateArtist);
 artistRouter.patch("/:id", artistController.updateArtist);
+artistRouter.delete("/:id", artistController.deleteArtist);
 module.exports = artistRouter;
