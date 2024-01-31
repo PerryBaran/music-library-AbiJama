a94b02d603533fff4b9913e7d967090527d4c125
Abi Jama <abi_jama88@hotmail.com>
2024-01-31 17:12:10 +0000

    had to resolve create artist as it stopped passing

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -2,14 +2,15 @@ const db = require("../db/index");
 
 const createArtist = async (req, res) => {
   const { name, genre } = req.body;
-  console.log(req.body);
+
   try {
-    await db.query(
+    const {
+      rows: [artist],
+    } = await db.query(
       `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`,
       [name, genre]
     );
-    console.log(name, "name");
-    res.status(201).json(name);
+    res.status(201).json(artist);
   } catch (err) {
     res.status(500).json(err.message);
   }
--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,4 +1,6 @@
+const { expect } = require("chai");
 const request = require("supertest");
+const db = require("../src/db");
 const app = require("../src/app");
 
 describe("create artist", () => {
@@ -6,20 +8,18 @@ describe("create artist", () => {
     describe("POST", () => {
       it("creates a new artist in the database", async () => {
         const { status, body } = await request(app).post("/artists").send({
-          name: "Tame impala",
+          name: "Tame Impala",
           genre: "rock",
         });
 
-        console.log("response:", status, body);
-
         expect(status).to.equal(201);
-        expect(body.name).to.equal("Tame impala");
+        expect(body.name).to.equal("Tame Impala");
         expect(body.genre).to.equal("rock");
 
         const {
           rows: [artistData],
         } = await db.query(`SELECT * FROM Artists WHERE id = ${body.id}`);
-        expect(artistData.name).to.equal("Tame impala");
+        expect(artistData.name).to.equal("Tame Impala");
         expect(artistData.genre).to.equal("rock");
       });
     });
